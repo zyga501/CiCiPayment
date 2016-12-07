81ab08120a39eaaf3a5ddfad23d373c86b32c797
VonChenPlus <VonChenPlus@gmail.com>
2016-12-07 14:54:36 +0800

    refactor paymethod

--- src/cc/database/merchant/PayMethod.java
+++ src/cc/database/merchant/PayMethod.java
@@ -1,6 +1,11 @@
 package cc.database.merchant;
 
 public class PayMethod {
+    public static void main(String[] args) throws Exception {
+        PayMethod payMethod = PayMethod.getPayMethodById(2);
+        System.out.print("");
+    }
+
     public static PayMethod getPayMethodById(int id) {
         String statement = "cc.database.merchant.mapping.payMethod.getPayMethodById";
         return Database.Instance().selectOne(statement, id);
@@ -38,8 +43,41 @@ public class PayMethod {
         this.method_ = method;
     }
 
+    public boolean isPrivateQualification() {
+        return privateQualification_;
+    }
+
+    public void setPrivateQualification(boolean privateQualification) {
+        this.privateQualification_ = privateQualification;
+    }
+
+    public PayType getPayType() {
+        return payType_;
+    }
+
+    public void setPayType(PayType payType) {
+        this.payType_ = payType;
+    }
+
+    public String getComment() {
+        return comment_;
+    }
+
+    public void setComment(String comment) {
+        this.comment_ = comment;
+    }
+
     private int id_;
     private String mode_;
     private long merchantId_;
     private String method_;
+    private boolean privateQualification_;
+    public enum PayType {
+        WEIXIN,
+        ALI,
+        JD,
+        BEST
+    };
+    private PayType payType_;
+    private String comment_;
 }
